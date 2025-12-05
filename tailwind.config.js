ce491d27da89f99c07867dcecd557c392fd51878
AmirMahdi <amirmahdisalamat6@gmail.com>
2025-12-05 23:08:34 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./src/**/*.{html,js}",
+  "./index.html"  // اگر index بیرون src باشه
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
